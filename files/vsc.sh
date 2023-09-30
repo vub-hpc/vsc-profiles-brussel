0c971da5e0239dd85397bd5c6ccbbfeab9e01fc7
Alex Domingo <alex.domingo.toro@vub.be>
2023-09-30 10:54:50 +0200

    rename HYDRA_MODULEPATH to more generic CLUSTER_MODULEPATH

--- files/vsc.sh
+++ files/vsc.sh
@@ -2,26 +2,26 @@ eval "$(/usr/bin/vsc_env bash)"
 
 export LMOD_SYSTEM_NAME="${VSC_INSTITUTE_CLUSTER}-${VSC_ARCH_LOCAL}${VSC_ARCH_SUFFIX:-}"
 
-HYDRA_MODULEPATH=
+CLUSTER_MODULEPATH=
 
 modulesroot="/apps/brussel/${VSC_OS_LOCAL}/${VSC_ARCH_LOCAL}${VSC_ARCH_SUFFIX:-}/modules"
 
 i=2019
 if [ -d "$modulesroot/${i}b/all" ]; then
-  HYDRA_MODULEPATH=$modulesroot/${i}b/all:$HYDRA_MODULEPATH
+  CLUSTER_MODULEPATH=$modulesroot/${i}b/all:$CLUSTER_MODULEPATH
 fi
 
 for i in {2020..2025}; do
   if [ -d "$modulesroot/${i}a/all" ]; then
-    HYDRA_MODULEPATH=$modulesroot/${i}a/all:$HYDRA_MODULEPATH
+    CLUSTER_MODULEPATH=$modulesroot/${i}a/all:$CLUSTER_MODULEPATH
   fi
   if [ -d "$modulesroot/${i}b/all" ]; then
-    HYDRA_MODULEPATH=$modulesroot/${i}b/all:$HYDRA_MODULEPATH
+    CLUSTER_MODULEPATH=$modulesroot/${i}b/all:$CLUSTER_MODULEPATH
   fi
 done
 
-export MODULEPATH=$HYDRA_MODULEPATH:/etc/modulefiles/vsc
+export MODULEPATH=$CLUSTER_MODULEPATH:/etc/modulefiles/vsc
 
-unset HYDRA_MODULEPATH
+unset CLUSTER_MODULEPATH
 
 # vim: set ft=sh:
