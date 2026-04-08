26e79b1e87f5dc8bbe48bc147d7d0caf36b6f4b0
Alex Domingo <alex.domingo.toro@vub.be>
2026-04-08 23:52:14 +0200

    ensure existence of authorized_keys file before injection

--- files/vscautokey.sh
+++ files/vscautokey.sh
@@ -3,7 +3,9 @@ myid=$(id -u)
 if [ "${myid:-0}" -ge "$minuid" ]; then
     if ! [ -a ~/.ssh/id_rsa ]; then
         if [ -w ~ ]; then
+            mkdir -p ~/.ssh
             ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N ""
+            touch ~/.ssh/authorized_keys
             chmod 600 ~/.ssh/authorized_keys
             cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
             chmod 400 ~/.ssh/authorized_keys ~/.ssh/id_rsa ~/.ssh/id_rsa.pub
